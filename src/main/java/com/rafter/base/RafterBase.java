0b27cf89a982baecefe3196d8f1a40ead135a8e1
Ajay <ajay191987@yahoo.in>
2023-02-10 14:00:04 +0530

    Latest changes

--- src/main/java/com/rafter/base/RafterBase.java
+++ src/main/java/com/rafter/base/RafterBase.java
@@ -9,19 +9,26 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class RafterBase {
 
 	public static WebDriver driver;
 	public static Properties prop;
-	
+	public static ExtentReports extent;
+	public static ExtentSparkReporter reporter;
+
 	public void initialization() {
-		//open the browser based on the value
+		// Configuring Browser
+		// open the browser based on the value
 		readPropertyFile();
 		driver = new ChromeDriver();
 		driver.get(prop.getProperty("url"));
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
 	}
-	
+
 	public void readPropertyFile() {
 		String propFilePath = "./src/main/java/com/rafter/config/config.properties";
 		prop = new Properties();
@@ -37,9 +44,22 @@ public class RafterBase {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 	}
-	
+
+	public void configureReport() {
+		// Configuring Extent Report
+		String reportPath = "./ExtentReports/ExtReport.html";
+		reporter = new ExtentSparkReporter(reportPath);
+		reporter.config().setDocumentTitle("Rafter Automation Testing Report");
+		reporter.config().setReportName("Automation Test Result");
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Environment", "QA");
+		extent.setSystemInfo("QA", "Ajay");
+		extent.setSystemInfo("Sprint", "Sprint 12");
+	}
+
 	public void tearDown() {
 		driver.quit();
 	}
--- src/test/java/com/rafter/testcases/LoginPageTest.java
+++ src/test/java/com/rafter/testcases/LoginPageTest.java
@@ -2,15 +2,19 @@ package com.rafter.testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.rafter.base.ExcelReader;
 import com.rafter.base.RafterBase;
 import com.rafter.pages.LoginPage;
 
-public class LoginPageTest {
+public class LoginPageTest extends RafterBase {
 
 	/*
 	 * Mistakes: 1. webdriver/browser config code everytime 2. Hard Coded the data
@@ -18,28 +22,36 @@ public class LoginPageTest {
 	 */
 
 	LoginPage lp;
-	
+
 	@BeforeMethod
 	public void browserConfig() {
 		lp = new LoginPage();
 		lp.initialization();
 	}
 
+	@BeforeTest
+	public void reportSetup() {
+		configureReport();
+	}
+
 	@Test
 	public void validateTitleTest() {
+		ExtentTest test = extent.createTest("Orange HRM Title Test Case");
 		String expTitle = "OrangeHRM";
 		Assert.assertEquals(lp.getPageTitle(), expTitle);
-
+		test.log(Status.PASS, "Marking TC Passed");
 	}
 
 	@Test
 	public void validateLoginTest() {
+		ExtentTest test = extent.createTest("Orange HRM Login Test Case");
 		lp.login();
 		String currentUrl = lp.getCurrentUrl();
 		Assert.assertTrue(currentUrl.contains("dashboard"));
+		test.log(Status.PASS, "Marking TC Passed");
 	}
-	
-	@Test
+
+	@Test(enabled = false)
 	public void forgotPasswordTest() {
 		// automate
 	}
@@ -48,7 +60,12 @@ public class LoginPageTest {
 	public void closeBrowserSetup() {
 		lp.tearDown();
 	}
-	
+
+	@AfterTest
+	public void generateReport() {
+		extent.flush();
+	}
+
 	@DataProvider
 	public Object[][] getUserData() {
 		ExcelReader reader = new ExcelReader("filename", "sheetname");
